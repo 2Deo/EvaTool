958c77acb02ed8343befcc297a92ba13996675ef
2Deo <ziemowit.holda@gmail.com>
2017-11-15 21:56:08 +0100

    Add fetch a batch action

--- src/actions/batch/fetch.js
+++ src/actions/batch/fetch.js
@@ -7,6 +7,7 @@ import {
 } from '../loading'
 
 export const FETCH_BATCHES = 'FETCH_BATCHES'
+export const FETCH_A_BATCH = 'FETCH_A_BATCH'
 
 const api = new API()
 
@@ -31,5 +32,28 @@ export default () => {
           payload: error.message
         })
       })
+  }
+}
+
+export const fetchABatch=(batchId) => {
+  return dispatch => {
+    dispatch({ type: APP_LOADING })
+
+  api.get(`/batches/${batchId}`)
+    .then((result) => {
+      dispatch({ type: APP_DONE_LOADING })
+      dispatch({ type: LOAD_SUCCESS })
+
+      dispatch({
+        type: FETCH_A_BATCH,
+        payload: result.body
+      })
+    })
+    .catch((error) => {
+      dispatch({ type: APP_DONE_LOADING })
+      dispatch({
+        type: LOAD_ERROR,
+        payload: error.message})
+    })
   }
 }
